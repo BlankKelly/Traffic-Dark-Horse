d10e4e101a47229058a9f2b41e28c15fc1e8963a
呆板的灰太狼 <m13573444349@163.com>
2022-05-01 22:49:16 +0800

    增加按调用量排序

--- src/main/java/com/myhub/traffic/dark/horse/statistics/RequestStatistics.java
+++ src/main/java/com/myhub/traffic/dark/horse/statistics/RequestStatistics.java
@@ -4,11 +4,9 @@ import com.myhub.traffic.dark.horse.core.ResourceSlidingWindowCache;
 import com.myhub.traffic.dark.horse.core.SlidingWindow;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 public class RequestStatistics {
     public static Map<String, Integer> statistics() {
@@ -20,11 +18,14 @@ public class RequestStatistics {
         while (iterator.hasNext()) {
             Map.Entry<@NonNull String, @NonNull Object> next = iterator.next();
             String key = next.getKey();
-            SlidingWindow slidingWindow = (SlidingWindow)next.getValue();
+            SlidingWindow slidingWindow = (SlidingWindow) next.getValue();
 
             countStatistics.put(key, slidingWindow.count());
         }
 
-        return countStatistics;
+        return countStatistics.entrySet()
+                .stream()
+                .sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, HashMap::new));
     }
 }
